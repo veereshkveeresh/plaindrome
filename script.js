4bd5ccc4a371cbe05d9f3d5244db1c9c88594f85
veereshkveeresh <veereshkuruva25@gmail.com>
2022-02-25 19:45:34 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 
-// plaindrome 
 function checkPalindrome(str) {
 
-
+    // find the length of a string
     const len = string.length;
 
+    // loop through half of the string
     for (let i = 0; i < len / 2; i++) {
 
-        
+       
         if (string[i] !== string[len - 1 - i]) {
             return 'It is not a palindrome';
         }
     }
-    return 'true';
+    return true;
 }
 
-
 const string = ('madam');
-
-
 const value = checkPalindrome(string);
+
 console.log(value);
